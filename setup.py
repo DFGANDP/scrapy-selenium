3e5e829e1784f098c35d4621e14ffd9e87092c4a
Wojtek AI <59583253+DFGANDP@users.noreply.github.com>
2024-06-26 18:27:43 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,6 @@
-"""This module contains the packaging routine for the pybook package"""
+"""This module contains the packaging routine for the package"""
 
 from setuptools import setup, find_packages
-try:
-    from pip.download import PipSession
-    from pip.req import parse_requirements
-except ImportError:
-    # It is quick hack to support pip 10 that has changed its internal
-    # structure of the modules.
-    from pip._internal.download import PipSession
-    from pip._internal.req.req_file import parse_requirements
-
 
 def get_requirements(source):
     """Get the requirements from the given ``source``
@@ -20,15 +11,12 @@ def get_requirements(source):
         The filename containing the requirements
 
     """
+    with open(source) as f:
+        requirements = f.read().splitlines()
 
-    install_reqs = parse_requirements(filename=source, session=PipSession())
-
-    return [str(ir.req) for ir in install_reqs]
-
+    return requirements
 
 setup(
     packages=find_packages(),
     install_requires=get_requirements('requirements/requirements.txt')
 )
-
-
